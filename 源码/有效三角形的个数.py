3db91885ca8f29f7012638d11e78898665fbf5a3
GQY <jidegaosuwo@163.com>
2024-09-21 10:07:19 +0800

    有效三角形个数

---
+++ 源码/有效三角形的个数.py
@@ -0,0 +1,26 @@
+class Effective_triangle():
+    def __init__(self,nums):
+        self.nums = nums
+    
+    def solution(self):
+
+        ans = 0
+        num = self.nums
+        num.sort()
+        n = len(num)
+
+        for i in range(n-1, 1, -1):
+            left = 0
+            right = i-1
+            while left < right:
+                if num[left] + num[right] > num[i]:
+                    ans += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return ans
+    
+nums = [4,2,3,4]
+a = Effective_triangle(nums)
+print(a.solution())
+
